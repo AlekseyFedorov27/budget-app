8c1a23ae96b416f0f81a1857ba91c86fd0f37bed
Aleksey <48358627+AlekseyFedorov27@users.noreply.github.com>
2020-01-21 17:24:05 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
 
-    <DialogConfirm :dialogVis="dialogVis" @closeDialog="showDialog" @deleteItem="deleteIt"/>
+    <DialogConfirm
+      :dialogVis="dialogVis"
+      @closeDialog="showDialog"
+      @deleteItem="deleteIt"
+    />
     <!-- <el-button type="text" @click="showDialog" >click to open the Dialog</el-button> -->
 
     <Form @submitForm="onFormSubmit" />
-    <TotalBalance :total="totalBalance" :style="{ color: totalBalanceColor }"/>
-    <BudgetList :list="list" @deleteItem="onDeleteItem" />
+    <TotalBalance :list="list" />
+    <BudgetList
+      :list="list"
+      @deleteItem="onDeleteItem"
+    />
 
   </div>
 </template>
@@ -17,14 +24,13 @@ import TotalBalance from "@/components/TotalBalance";
 import Form from "@/components/Form";
 import DialogConfirm from "@/components/DialogConfirm";
 
-
 export default {
   name: "app",
   components: {
     BudgetList,
     TotalBalance,
     Form,
-    DialogConfirm,
+    DialogConfirm
   },
   data: () => ({
     list: {
@@ -41,39 +47,13 @@ export default {
         id: 2
       }
     },
-    dialogVis: {type: false, listItemDel: Number},
+    dialogVis: { type: false, listItemDel: Number }
   }),
-  computed: {
-    totalBalance() {
-      return Object.values(this.list).reduce(
-        (acc, item) => acc + this.plusMinus(item.type) * item.value,
-        0
-      )
-    },
-    totalBalanceColor(context){
-// 3. TotalBalance должен менять цвет текста в зависимости от значения total, 
-// если значение больше нуля то цвет  зеленый, если ноль то черный и если 
-// меньше нуля то красный.
-      // console.log( comp )
-      if( context.totalBalance > 0 ){
-        return 'green'
-      }if ( context.totalBalance == 0 ){
-        return 'black'
-      }
-        return 'red'
-    }
-  },
+  computed: {},
   methods: {
-// 2. Сейчас есть бага, если мы выберем в форме тип outcome но
-// цифру введем без минуса то это значение приплюсуется с общему балансу,
-//  нужно это исправить.
-    plusMinus(type){
-      return ( type === "INCOME" ? 1 : -1 ) 
-    },   
     onDeleteItem(id) {
-        this.showDialog();
-        this.dialogVis.listItemDel = this.list[id]
-        
+      this.showDialog();
+      this.dialogVis.listItemDel = this.list[id];
     },
     onFormSubmit(data) {
       const newObj = {
@@ -82,10 +62,10 @@ export default {
       };
       this.$set(this.list, newObj.id, newObj);
     },
-    showDialog(){
-      this.dialogVis.type=!this.dialogVis.type
+    showDialog() {
+      this.dialogVis.type = !this.dialogVis.type;
     },
-    deleteIt(id){
+    deleteIt(id) {
       // console.log('delete', id)
       this.$delete(this.list, id.id);
       this.showDialog();
--- src/components/TotalBalance.vue
+++ src/components/TotalBalance.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="total-value">Balance: {{ total }}</div>
+  <div
+    class="total-value"
+    :style="{ color: totalBalanceColor }"
+  >Balance: {{ totalBalance }}</div>
 </template>
 
 <script>
 export default {
   name: "TotalBalance",
   props: {
-    total: {
-      type: Number,
-      default: 0
+    list: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    totalBalance() {
+      return Object.values(this.list).reduce(
+        (acc, item) => acc + this.plusMinus(item.type) * item.value,
+        0
+      );
+    },
+    totalBalanceColor() {
+      // 3. TotalBalance должен менять цвет текста в зависимости от значения total,
+      // если значение больше нуля то цвет  зеленый, если ноль то черный и если
+      // меньше нуля то красный.
+      // console.log( comp )
+      if (this.totalBalance > 0) {
+        return "green";
+      }
+      if (this.totalBalance == 0) {
+        return "black";
+      }
+      return "red";
+    }
+  },
+  methods: {
+    // 2. Сейчас есть бага, если мы выберем в форме тип outcome но
+    // цифру введем без минуса то это значение приплюсуется с общему балансу,
+    //  нужно это исправить.
+    plusMinus(type) {
+      return type === "INCOME" ? 1 : -1;
     }
   }
 };
